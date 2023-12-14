293e67199f806085c19a14da8316bf1bfd20f510
tonmoymojumder <74363531+tonmoy998@users.noreply.github.com>
2023-12-14 16:48:15 +0600

    Updating Noise Cancellation

--- noise-can.sh
+++ noise-can.sh
@@ -1,3 +1,5 @@
+sudo apt install pulseaudio 
+pulseaudio --start
 sudo cp /etc/pulse/default.pa /etc/pulse/default.pa.bak
 sudo cat <<EOT >> /etc/pulse/default.pa
 load-module module-echo-cancel source_name=noechosource sink_name=noechosink
